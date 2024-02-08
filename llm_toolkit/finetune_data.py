484055d4ccdffd8cd7bfe3fd90322e6bc9efae65
sallywang147 <sallyjunsongwang@gmail.com>
2024-02-08 07:56:46 +0000

    training data

--- llm_toolkit/finetune_data.py
+++ llm_toolkit/finetune_data.py
@@ -17,56 +17,124 @@ def find_function(file, first, last):
     except ValueError:
         return ""
 
+def _make_w_io_base(f, mode: str):
+    if not isinstance(f, io.IOBase):
+        f_dirname = os.path.dirname(f)
+        if f_dirname != "":
+            os.makedirs(f_dirname, exist_ok=True)
+        f = open(f, mode=mode)
+    return f
 
-directory = '/home/sallyjunsongwang/llmfz/comparative_results_gpt4'
+
+def _make_r_io_base(f, mode: str):
+    if not isinstance(f, io.IOBase):
+        f = open(f, mode=mode)
+    return f
 
 
-for (root, directories, files) in os.walk(directory, topdown=False):
-        for name in files:
-                if name == "prompt.txt":
-                        prompt_file = os.path.join(root, name)
-                        with open(prompt_file, "r") as file:
-                                file =  file.read()
-                                first = '<function header>'
-                                last = '</function header>'
-                                function = find_function(file, first, last)
-                                print(f"This is the function signature:  {function}\n")
-        for name in directories:
-                if name == "fixed_targets": 
-                        targets_dir = os.path.join(root, name)
-                        print(f"target file: {targets_dir}\n")
-                        for target in os.listdir(targets_dir):
-                                possible_target = os.path.join(targets_dir, target)
-                                if os.path.isfile(possible_target):
-                                        with open(possible_target, "r") as target_file:
-                                                tf = target_file.read()
-                                                print(f"target file : {tf}\n")
-         
-'''
-with open("/home/sallyjunsongwang/llmfz/comparative_results_gpt4/output-avahi-avahi_dns_packet_append_record/prompt.txt", "r") as file:
-        #print(file.read().replace('\\n','\n'))
-        
-        for line in file.read().split('\\n'):
-                print(line)
-                print('\n')
-        
-        test_str =  file.read()
-        print(test_str)
-        print('\n')
-        # initializing substrings
-        sub1 = '<function header>'
-        sub2 = '</function header>'
-    
-        # getting index of substrings
-        res = find_between_r(test_str, sub1, sub2)
+def jdump(obj, f, mode="w", indent=4, default=str):
+    f = _make_w_io_base(f, mode)
+    if isinstance(obj, (dict, list)):
+        json.dump(obj, f, indent=indent, default=default)
+    elif isinstance(obj, str):
+        f.write(obj)
+    else:
+        raise ValueError(f"Unexpected type: {type(obj)}")
+    f.close()
 
-        # printing result
-        print("The extracted string : " + res)
+def gpt4_data_prep(result_dir,train_file): 
+        data_list = []   
+        train_file_path = os.path.join(result_dir, train_file)
+        for (root, directories, files) in os.walk(result_dir, topdown=False):
+                data = OrderedDict()
+                prompt = ""
+                for name in files:
+                        if name == "prompt.txt":
+                                prompt_file = os.path.join(root, name)
+                                with open(prompt_file, "r") as file:
+                                        prompt =  file.read()                                                                                                                                
+                for name in directories:
+                        if name == "fixed_targets": 
+                                targets_dir = os.path.join(root, name)
+                                for target in os.listdir(targets_dir):
+                                        possible_target = os.path.join(targets_dir, target)
+                                        if os.path.isfile(possible_target):
+                                                with open(possible_target, "r") as target_file:
+                                                        single_target = target_file.read()
+                                                        data["prompt"] = prompt                                                          
+                                                        data["completion"] = single_target
+                                                        data_list.append(data)
+        jdump(data_list, train_file_path)   
 
-#def contextualized_data_prep(prompt, target):
+def one_step_data_prep(result_dir,train_file): 
+        data_list = []   
+        train_file_path = os.path.join(result_dir, train_file)
+        for (root, directories, files) in os.walk(result_dir, topdown=False):
+                data = OrderedDict()
+                for name in files:
+                        if name == "prompt.txt":
+                                prompt_file = os.path.join(root, name)
+                                with open(prompt_file, "r") as file:
+                                        file =  file.read()
+                                        first = "<function header>"
+                                        last = "</function header>"
+                                        function = find_function(file, first, last)                                                                                                                                       
+                for name in directories:
+                        if name == "fixed_targets": 
+                                targets_dir = os.path.join(root, name)
+                                for target in os.listdir(targets_dir):
+                                        possible_target = os.path.join(targets_dir, target)
+                                        if os.path.isfile(possible_target):
+                                                with open(possible_target, "r") as target_file:
+                                                        single_target = target_file.read()
+                                                        data["prompt"] = "function signature is: " + function.strip()                                                          
+                                                        data["completion"] = single_target
+                                                        data_list.append(data)
+        jdump(data_list, train_file_path)                                   
 
-#def main():
+def contextualized_data_prep(result_dir,train_file):
+        train_file_path = os.path.join(result_dir, train_file)
+        train_data = open(train_file_path, "a+") 
+        first_thought = ""
+        second_thought = ""
+        third_thought = ""
+        for (root, directories, files) in os.walk(result_dir, topdown=False):
+                for name in files:
+                        if name == "prompt.txt":
+                                prompt_file = os.path.join(root, name)
+                                with open(prompt_file, "r") as file:
+                                        file =  file.read()
+                                        first = "<function header>"
+                                        last = "</function header>"
+                                        function = find_function(file, first, last)
+                                        first_thought = "This is the function signature: " + \
+                                        function.strip() + "\n"
+                                        second_thought = file  + "\n"
+                               
+                for name in directories:
+                        if name == "fixed_targets": 
+                                targets_dir = os.path.join(root, name)
+                                for target in os.listdir(targets_dir):
+                                        possible_target = os.path.join(targets_dir, target)
+                                        if os.path.isfile(possible_target):
+                                                with open(possible_target, "r") as target_file:
+                                                        single_target = target_file.read()
+                                                        third_thought = "function fuzz target(s) are: " + \
+                                                        single_target + "\n"
+                if first_thought != "" and  second_thought != "" and third_thought != "":                                    
+                        one_sample = first_thought + second_thought + third_thought + "<end of text>" + "\n"
+                        train_data.write(one_sample)
+        train_data.close()
 
-#if __name__ == "__main__":
-#       main()    
-'''
+RESULT_DIR = '/home/sallyjunsongwang/llmfz/comparative_results_gpt4'
+ONE_STEP = "onestep.json"
+GPT4_DATA = "gpt4.json"
+CONTEXTUALIZED = "context.txt"
+
+def main():
+        contextualized_data_prep(RESULT_DIR, CONTEXTUALIZED)
+        one_step_data_prep(RESULT_DIR, ONE_STEP)
+        gpt4_data_prep(RESULT_DIR, GPT4_DATA)
+
+if __name__ == "__main__":
+       main()    
